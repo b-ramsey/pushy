8916e1703c2457c002e0d630a0f3268303dcd11e
Benjamin Ramsey <benjamin.james.ramsey@gmail.com>
2015-08-06 17:12:18 +0930

    Add wait attribute to the pushy LWRP resource

--- resources/default.rb
+++ resources/default.rb
@@ -2,3 +2,4 @@ actions :run
 attribute :job_name, :kind_of => String, :name_attribute => true, :required => true
 attribute :nodes, :kind_of => Array, :required => true
 attribute :require_success, :kind_of => [FalseClass, TrueClass], :default => false
+attribute :wait, :kind_of => [FalseClass, TrueClass], :default => false
