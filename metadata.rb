f3b1a63e90745fcd03f9ea4391ba647d3873fd3f
Benjamin Ramsey <benjamin.james.ramsey@gmail.com>
2015-08-06 17:22:19 +0930

    Update version numbering for pushy cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'Michael Ducy <michael@opscode.com>'
 license          'apachev2'
 description      'Installs/Configures lwrp for pushy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
